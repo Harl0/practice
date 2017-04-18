0f545f1c216cb88a30a7cd519552ecababafdcd0
Jason Harris <jiharris90@gmail.com>
2017-04-18 18:25:49 +0100

    FizzBuzz refactor

--- src/main/scala/katas/FizzBuzz.scala
+++ src/main/scala/katas/FizzBuzz.scala
@@ -5,11 +5,22 @@ package katas
   */
 object FizzBuzz {
 
-def getResult(number: Int) :String = (number % 3, number % 5) match {
-  case (0 , 0) => "fizzbuzz"
-  case (0, _) => "fizz"
-  case (_, 0) => "buzz"
-  case _ => number.toString
-}
+  def getResult(number: Int): String = (number % 3, number % 5) match {
+    case (0, 0) => "fizzbuzz"
+    case (0, _) => "fizz"
+    case (_, 0) => "buzz"
+    case _ => number.toString
+  }
+
+  def getResultIf(x: Int) = {
+    if (x % 15 == 0)
+      "FizzBuzz"
+    else if (x % 3 == 0)
+      "Fizz"
+    else if (x % 5 == 0)
+      "Buzz"
+    else
+      x
+  }
 
 }
